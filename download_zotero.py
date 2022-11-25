4700dab76c1b62f9bd63ff87f7ddd2798bff4150
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-11-25 09:58:54 +0100

    added outdated entities files; added zotero workflow

---
+++ download_zotero.py
@@ -0,0 +1,6 @@
+from zotero_utils import get_listbibl, update_listbibl
+
+list_bibl = "./out/listbibl.xml"
+
+new_doc = update_listbibl(get_listbibl(limit="50"), orig_file=list_bibl)
+new_doc.tree_to_file(list_bibl)
